a207e24be6be24b7c8fca9f49a88c43738d52362
ivanaperich <46377891+ivanaperich@users.noreply.github.com>
2019-04-17 16:14:21 -0400

    Add files via upload

---
+++ taller.dao/src/taller/dao/TallerDao.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller.dao;
+
+import vista.VentanaInicio;
+
+/**
+ *
+ * @author usuario
+ */
+public class TallerDao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        VentanaInicio vi= new VentanaInicio();
+        vi.setVisible(true);
+    }
+    
+}
